6eebf5700e0c7f8266e085dfcded717d2585332c
lilysashad <lilysdeng@gmail.com>
2022-01-16 21:55:48 -0500

    Project files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SEVENTEEN-Quiz
-A simple HTML/Javascript trivia game about K-pop boy group SEVENTEEN
+
+<img src='/images/seventeen_pic.jpeg' title='Group photo of SEVENTEEN' width='30%' alt='Group photo of 13 East-Asian boys that comprise of Korean boy group Seventeen' />
+
+A simple 10-question trivia game about K-pop boy group SEVENTEEN, built with HTML, CSS, and Javascript.
+
+Can be played by clicking on answer choices with your mouse. Every correct answer earns you 1 point. However, there is a 15-second time limit for each question.
+
+Time spent: **5** hours spent in total
+
+# Attributes
+
+Learning project for my Tech Incubator internship
+
+Tutorial videos have been split into [part 1](https://www.youtube.com/watch?v=pQr4O1OITJo) and [part 2](https://www.youtube.com/watch?v=WUBhpSRS_fk)
---
+++ js/questions.js
@@ -0,0 +1,103 @@
+let questions = [
+    {
+        numb: 1,
+        question:"How many members are in SEVENTEEN?",
+        answer:"13",
+        options:[
+            "13",
+            "17",
+            "10"
+        ]
+    },
+    {
+        numb: 2,
+        question:"What is member Seungkwan's family name?",
+        answer:"Boo",
+        options:[
+            "Boo",
+            "Lee",
+            "Choi"
+        ]
+    },
+    {
+        numb: 3,
+        question:"Which company is SEVENTEEN under?",
+        answer:"Pledis Entertainment",
+        options:[
+            "Pledis Entertainment",
+            "SM Entertainment",
+            "Starship Entertainment"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Who is the main producer of SEVENTEEN?",
+        answer:"Woozi",
+        options:[
+            "S.Coups",
+            "Dino",
+            "Woozi"
+        ]
+    },
+    {
+        numb: 5,
+        question:"Where was member Vernon born?",
+        answer:"New York",
+        options:[
+            "California",
+            "New York",
+            "Virginia"
+        ]
+    },
+    {
+        numb: 6,
+        question:"Which of the following choices is NOT a Hip Hop Unit song?",
+        answer:"Swimming Fool",
+        options:[
+            "Check-In",
+            "If I",
+            "Swimming Fool"
+        ]
+    },
+    {
+        numb: 7,
+        question:"Which track was nominated for the 2021 MTV Video Music Awards?",
+        answer:"Ready to Love",
+        options:[
+            "Ready to Love",
+            "Not Alone (Hitorijanai)",
+            "Rock With You"
+        ]
+    },
+    {
+        numb: 8,
+        question:"SEVENTEEN has a YouTube series called",
+        answer:"Going SEVENTEEN",
+        options:[
+            "RUN SEVENTEEN",
+            "Going SEVENTEEN",
+            "SEVENTEEN Live"
+        ]
+    },
+    {
+        numb: 9,
+        question:"How tall is member Mingyu?",
+        answer:"6'2”",
+        options:[
+            "5’10”",
+            "6’0”",
+            "6'2”"
+        ]
+    },
+    {
+        numb: 10,
+        question:"Member Hoshi's solo debut is titled",
+        answer:"Spider",
+        options:[
+            "Eye of the Tiger",
+            "Spider",
+            "Tarantula"
+        ]
+    }
+
+];
